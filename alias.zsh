c4e0ae28831ef552bc85af228e29babbd8cfb6fd
MiniZagGi <33091100+MiniZagGi@users.noreply.github.com>
2025-12-30 18:24:25 +0100

    feat: Add new aliases for local IP and desktop visibility

--- alias.zsh
+++ alias.zsh
@@ -2,9 +2,19 @@
 
 # Gets public IPv4 and IPv6 address
 alias getip="curl 'https://api64.ipify.org?format=json'"
+# Gets the IP set for Wi-Fi interface
+alias localip="ipconfig getifaddr en0"
+
 alias week='date +%V'
+# Directory aliases
 alias pj='$HOME/Documents/Projects'
 alias dot='$HOME/.dotfiles'
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+
 alias flushdns='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
 # Created by `pipx` on 2024-04-21 17:42:48
 export PATH="$PATH:/Users/mini/.local/bin"
